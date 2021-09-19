caf69a1e20eebcf4c8bf13e8996e593b88777b2f
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-09-18 21:03:47 -0500

    displayed GalleryList on DOM

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,39 @@
-import React, { useState } from 'react';
+import Axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import galleryItems from '../../modules/gallery.data';
 import './App.css';
 
 function App() {
 
-  const [GalleryList, setGalleryList] = useState ([]);
+  const [galleryList, setGalleryList] = useState (galleryItems);
+  const [counter, setCounter] = useState(0);
+
+  const fetchImages = () => {
+    Axios({
+      method: 'GET',
+      url: '/gallery',
+    }).then((response) => {
+      console.log(response.data);
+      setGalleryList(response.data);
+    }).catch((error) => {
+      console.log(error);
+      alert('error in making GET request', error);
+    });
+  }
 
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>May Gallery</p>
+        {/* <p>My Gallery</p> */}
+        {galleryItems.map(image => <img key={image.id} src={image.path}/>)}
         <div className="gallery-image">
           <img src="images/climbing-2.jpg"/>
           <br /><br />
-          <button className="love-button">love it!</button>
+          <button onClick={() => setCounter(counter + 1)}  className="love-button">love it!</button>
           <br />
-          <p>&#10084;&#65039;&nbsp; 125</p>
+          <p>&#10084;&#65039;&nbsp; {counter}</p>
         </div>
       </div>
     );
---
+++ src/modules/gallery.data.js
@@ -0,0 +1,10 @@
+const galleryItems = [
+    { id: 1, path: 'images/climbing-2.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/climbing-3.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 3, path: 'images/food-3.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 4, path: 'images/food-5.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 5, path: 'images/nature-2.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 6, path: 'images/travel-3.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+];
+
+module.exports = galleryItems;
