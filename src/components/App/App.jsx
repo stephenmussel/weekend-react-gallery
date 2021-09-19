c3137f6c4bb3e198c7f9450a641f4909d4c92398
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-09-18 19:42:15 -0500

    added mocked up gallery item

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [GalleryList, setGalleryList] = useState ([]);
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <p>May Gallery</p>
+        <div className="gallery-image">
+          <img src="images/climbing-2.jpg"/>
+          <br /><br />
+          <button className="love-button">love it!</button>
+          <br />
+          <p>&#10084;&#65039;&nbsp; 125</p>
+        </div>
       </div>
     );
 }
