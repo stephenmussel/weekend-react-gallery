a26f692d1e7735bec980de8ab1cfa8c6d2fa3ac3
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-09-19 09:43:19 -0500

    started over; moved header into component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,42 +1,17 @@
-import Axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import galleryItems from '../../modules/gallery.data';
+import React from 'react';
 import './App.css';
-import Header from '../Header/Header.jsx';
-import GalleryList from '../GalleryList/GalleryList.jsx';
-import GalleryItem from '../GalleryItem/GalleryItem.jsx';
+import Header from '../Header/Header';
 
 function App() {
 
-  const [galleryList, setGalleryList] = useState ([]);
-
-  // fetches images on page load
-  useEffect(() => {
-    fetchImages()
-  }, [])
-
-  const fetchImages = () => {
-    Axios({
-      method: 'GET',
-      url: '/gallery',
-    }).then((response) => {
-      console.log(response.data);
-      setGalleryList(response.data);
-    }).catch((error) => {
-      console.log(error);
-      alert('error in making GET request', error);
-    });
-  }
 
     return (
       <div className="App">
         <Header />
-        {/* <GalleryList 
-          galleryItems={galleryItems}
-        /> */}
-        <GalleryItem />
+        <p>Gallery goes here</p>
+        <img src="images/goat_small.jpg"/>
       </div>
-    )
+    );
 }
 
 export default App;
