50cc03f284296f4700e54a89913086807956976c
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-09-19 10:32:42 -0500

    retrieved data from /gallery

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+const galleryItems = require('../../src/modules/gallery.data');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import axios from 'axios';
+import { useState } from 'react';
+import galleryItems from '../../modules/gallery.data';
+import { useEffect } from 'react/cjs/react.development';
 
 function App() {
 
+  const [galleryList, setGalleryList] = useState(galleryItems);
+  const [likeCounter, setLikeCounter] = useState(0);
+
+  const fetchGallery = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery',
+    }).then((response) => {
+      console.log(response.data);
+      setGalleryList(response.data);
+    }).catch((error) => {
+      console.log('error in fetchGallery', error);
+    });
+  }
+
+  useEffect(() => {
+    fetchGallery();
+  }, []);
 
     return (
       <div className="App">
         <Header />
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <h2>Gallery goes here</h2>
+        <h2><img src="{galleryItems.path}" /></h2>
+
+        <p>Gallery item</p>
+        <img src="images/climbing-2.jpg"/>
+        <button className="love-button" onClick={ () => setLikeCounter(likeCounter + 1) }>love it!</button>
+        <div>&#10084;&#65039;&nbsp; {likeCounter}</div>
       </div>
     );
 }
